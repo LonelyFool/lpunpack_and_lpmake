06e6915229d381bc3a2f6bbb5dd348741f394ba7
LonelyFool <2655294004@qq.com>
2021-07-18 05:33:44 +0000

    android super.img tools

--- lib/protobuf/src/google/protobuf/compiler/javanano/javanano_main.cc
+++
@@ -1,8 +0,0 @@
-#include <google/protobuf/compiler/code_generator.h>
-#include <google/protobuf/compiler/javanano/javanano_generator.h>
-#include <google/protobuf/compiler/plugin.h>
-
-int main(int argc, char* argv[]) {
-  google::protobuf::compiler::javanano::JavaNanoGenerator generator;
-  return google::protobuf::compiler::PluginMain(argc, argv, &generator);
-}
